945866b504b1aa7c18e78b1a01beca151a53654c
st10066248 <st10066248@vcconnect.edu.za>
2024-03-29 22:16:27 +0200

    Add project files.

---
+++ KhumaloCraft Emporium/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using KhumaloCraft_Emporium.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KhumaloCraft_Emporium.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact() 
+        {  
+            return View();  
+        }
+
+        public IActionResult Work()
+        {
+            return View("Work");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KhumaloCraft Emporium/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KhumaloCraft_Emporium.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
